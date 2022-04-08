31d397b7c360f32bc0a7eb82f3280b37132bde10
Mammi17 <hadjaramamane61@gmail.com>
2022-04-07 18:28:47 -0700

    the task 1-6

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,16 @@
+#!/bin/bash
+#include<stdio.h>
+main ()
+{
+	int a;
+	long int b;
+	long long int c;
+	char d;
+	float e;
+	printf("Size of an int is: %lu byte(s)\n", (unsigned long)sizeof(a));
+	printf("Size of a long int is: %lu byte(s)\n",(unsigned long)sizeof(b));
+	printf("Size of a long long int is: %lu byte(s)\n",(unsigned long)sizeof(c));
+	printf("Size of a char is: %lu byte(s)\n",(unsigned long)sizeof(d));
+	printf("Size of a float is: %lu byte(s)\n",(unsigned)sizeof(e));
+	return(0);
+}
