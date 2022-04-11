491cf804db67d23fa47acf583d09562c30a745d2
Mammi17 <hadjaramamane61@gmail.com>
2022-04-11 16:46:27 -0700

    the task 10-12

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -13,20 +13,19 @@ int main(void)
 	{
 		for (digits = digit + 1; digits <= 8; digits++)
 		{
+			for (digit1 = digits + 1; digit1 <= 9; digit1++)
 			{
-				for (digit1 = digits + 1; digit1 <= 9; digit1++)
-				{
-					putchar((digit % 10) + '0');
-					putchar((digits % 10) + '0');
-					putchar((digit1 % 10) + '0');
-					if (digit == 7 && digits == 8 && digit1 == 9)
-						continue;
-					putchar(',');
-					putchar(' ');
-				}
+				putchar((digit % 10) + '0');
+				putchar((digits % 10) + '0');
+				putchar((digit1 % 10) + '0');
+				if (digit == 7 && digits == 8 && digit1 == 9)
+					continue;
+				putchar(',');
+				putchar(' ');
 			}
 		}
-
+	}
+	
 	putchar('\n');
 
 	return (0);
