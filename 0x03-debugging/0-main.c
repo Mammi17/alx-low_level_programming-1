23279d9246ecdd7353cfb2ab1702ba9036714e3a
Mammi17 <hadjaramamane61@gmail.com>
2022-04-15 12:45:57 -0700

    task 0

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,13 @@
+#includ "main.h"
+/**
+ * main - main
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int i;
+	i=0;
+	positive_or_negative(i);
+	return (0);
+}
---
+++ 0x03-debugging/main.h
@@ -0,0 +1,10 @@
+#ifndef MAIN.H
+#define MAIN.H
+#include<stdio.h>
+
+void positive_or_negative(int a);
+int largest_number(int i, int b, int c);
+void print_remaining_days(int mm, int dd, int aa);
+int convert_day(int mm, int dd);
+
+#endif
