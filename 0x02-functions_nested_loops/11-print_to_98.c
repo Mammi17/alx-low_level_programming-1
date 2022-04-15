0db3369c6985d9031cfa3198239bfc19d139aefd
Mammi17 <hadjaramamane61@gmail.com>
2022-04-14 18:34:58 -0700

    task 0-12

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -8,20 +8,29 @@
  */
 void print_to_98(int n)
 {
-	if (n >= 98)
+	int i;
+
+	if (n > 98)
 	{
-		while (n > 98)
+		for (i=n; i > 97; i--)
 		{
-			printf("%d", n--);
-			printf("%d\n", n);
+			printf("%d", i);
+			if (i != 98)
+			{
+				printf(", ");
+			}
 		}
 	}
 	else
 	{
-		while (n < 98)
+		for (i = n; i < 99; i++)
 		{
-			printf("%d, ", n++);
-			printf("%d\n", n);
+			printf("%d", i);
+			if (i != 98)
+			{
+				printf(", ");
+			}
 		}
 	}
+	printf("\n");
 }
