d2dc4cab1a77e5317a099cf939c50cd508077a1d
Mammi17 <hadjaramamane61@gmail.com>
2022-04-16 20:17:32 -0700

    task 0-12

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -10,27 +10,26 @@ int main(void)
 	int i;
 
 	for (i = 1; i <= 100; i++)
+	{
+		if (i % 3 == 0)
 		{
-			if ((i % 3 == 0) && (i % 5 == 0))
-			{
-				printf("FizzBuzz");
-			}
-			else if (i % 3 == 0)
-			{
-				printf("Fizz");
-			}
-			else if (i % 5 == 0)
-			{
-				printf("Buzz");
-			}
-
-			else
-			{
-				printf("%i", i);
-			}
-			putchar(' ');
-			
+			printf("Fizz");
 		}
+		else if (i % 5 == 0)
+		{
+			printf("Buzz");
+		}
+		else if ((i % 3 == 0) && (i % 5 == 0))
+		{
+			printf("FizzBuzz");
+		}
+
+		else
+		{
+			printf("%i", i);
+		}
+		putchar(' ');
+	}
 	printf("\n");
 	return (0);
 }
