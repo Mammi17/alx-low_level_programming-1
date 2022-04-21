7d406211affd53bcd279e231a8b9c023b4050456
Mammi17 <hadjaramamane61@gmail.com>
2022-04-20 21:46:53 -0700

    task8-11

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,30 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+/**
+ * main - random password generator for 101-crackme
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	char i[200];
+	int a, b, c;
+
+	b = 0;
+	c = 0;
+	srand(time(NULL));
+	while (b < 3000)
+	{
+		a = rand() % 122;
+		if (a > 32)
+		{
+			i[c++] = a;
+			b += a;
+		}
+	}
+	a[c++] = (3127 - b);
+	a[c] = '\0';
+	printf("%s", i);
+	return (0);
+}
--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -1,3 +1,21 @@
 #include "main.h"
 /**
- *
+ * _strcpy - copies src to dest
+ * @dest: char
+ * @src: char
+ * Return: char
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int a;
+
+	a = 0;
+	while (src[a])
+	{
+		dest[a] = scr[a];
+		a++;
+	}
+	dest[a] = '\0';
+	return (dest);
+}
