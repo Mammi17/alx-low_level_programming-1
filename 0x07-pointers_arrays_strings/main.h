7dd08ea85b9bec233b5c55ac31a0fd612e894034
Mammi17 <hadjaramamane61@gmail.com>
2022-04-26 19:02:46 -0700

    task 0-7

---
+++ 0x07-pointers_arrays_strings/main.h
@@ -0,0 +1,14 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+char *_memset(char *s, char b, unsigned int n);
+char *_memcpy(char *dest, char *src, unsigned int n);
+char *_strchr(char *s, char c);
+unsigned int _strspn(char *s, char 8accept);
+char *_strpbrk(char *s, char *accept);
+void print_cheesboard(char (*a)[8]);
+char *_strstr(char *haystack, char *needle);
+void print_diagsums(int *a, int size);
+int _putchar(char c);
+#endif /* MAIN_H*/
+
