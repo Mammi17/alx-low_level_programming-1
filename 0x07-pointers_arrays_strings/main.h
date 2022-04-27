8eaf0d84b619e5179a478e02d6f8971a74fd1c58
Mammi17 <hadjaramamane61@gmail.com>
2022-04-26 19:59:11 -0700

    task 0-7

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -4,7 +4,7 @@
 char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
 char *_strchr(char *s, char c);
-unsigned int _strspn(char *s, char 8accept);
+unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 void print_cheesboard(char (*a)[8]);
 char *_strstr(char *haystack, char *needle);
