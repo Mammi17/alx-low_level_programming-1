2ddef232f11ec0fcd079d0892b11d9f86295c23d
Mammi17 <hadjaramamane61@gmail.com>
2022-04-28 19:13:43 -0700

    task 0-6

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -10,9 +10,13 @@ int factorial(int n)
 	int h;
 
 	if (n == 0)
+	{
 		return (1);
+	}
 	else if (n < 0)
+	{
 		return (-1);
+	}
 	else
 	{
 		h = n * factorial(n - 1);
--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -17,5 +17,5 @@ int is_prime_number(int n)
 		else
 			return (0);
 	}
-	return (is_prime_number(n+1));
+	return (is_prime_number(n));
 }
