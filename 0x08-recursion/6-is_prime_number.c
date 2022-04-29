f24884035bf63f554f3b15412d516dd353d77ded
Mammi17 <hadjaramamane61@gmail.com>
2022-04-28 19:27:27 -0700

    task 0-6

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -10,12 +10,14 @@ int is_prime_number(int n)
 {
 	int i;
 
-	for (i = 2; i!= n; i++)
-	{
-		if (n % i == 0)
-			return (1);
-		else
-			return (0);
-	}
+	i = 2;
+	if (n == i)
+		return (1);
+	else if (n == 0 || n == 1)
+		return (0);
+	else if (n % i == 0)
+		return (1);
+	else
+		return (0);
 	return (is_prime_number(n));
 }
