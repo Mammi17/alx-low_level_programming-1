d12bdd97b458a83dfc0a270a9fc76f57a4f77f29
Hadjara <hadjaramamane61@gmail.com>
2022-08-29 14:14:09 -0700

    task 0

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 /**
  * main - prints the name of program
- * argc: an integer
- * argv: a pointer
+ * @argc: an integer
+ * @argv: a pointer
  * Return: 0
  */
 
-int main(int argc, char **argv)
+int main (char **argv)
 {
 	printf("The name of the program is:%s\n", argv[0]);
 	return (0);
