a2a525d4e367839fc56f26bfc9ad381dad77cc48
Hadjara <hadjaramamane61@gmail.com>
2022-08-29 17:49:44 -0700

    task 0

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -7,7 +7,7 @@
  * Return: 0
  */
 
-int main (int argc _attribute_((unused)), char **argv)
+int main (int argc __attribute__((unused)), char **argv)
 {
 	printf("The name of the program is: %s\n", argv[0]);
 	return (0);
