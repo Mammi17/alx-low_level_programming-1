0dd59575f047bf616ca6e633e72d9359f08f7a9e
Hadjara <hadjaramamane61@gmail.com>
2022-08-31 16:39:56 -0700

    task 1-3

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -13,13 +13,13 @@ int main(int argc, char **argv)
 	int mul, n1, n2;
 
 	mul = 0;
-	n1 = atoi(argv[1]);
-	n2 = atoi(argv[2]);
 	if (argc != 3)
 	{
 		printf("%s\n", "Error");
 		return (1);
 	}
+	n1 = atoi(argv[1]);
+	n2 = atoi(argv[2]);
 	mul = n1 * n2;
 	printf("%d\n", mul);
 	return (0);
