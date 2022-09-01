38fadfb4fa2182964c6cd6bde3ab38a16fe0af43
Hadjara <hadjaramamane61@gmail.com>
2022-08-31 18:34:45 -0700

    task 4

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -18,7 +18,7 @@ int main(int argc, char **argv)
 	}
 	for (i = 1; i < argc; i++)
 	{
-		for (k = 1;  argv[i][k] != '\0'; k++)
+		for (k = 0;  argv[i][k] != '\0'; k++)
 		{
 			if (argv[i][k] < '0' || argv[i][k] > '9')
 			{
