d77e8abc2bfdc13a6e80ca7ac173742cb6a12420
Hadjara <hadjaramamane61@gmail.com>
2022-11-17 16:39:57 -0800

    task 5

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -11,8 +11,7 @@
 int main(int argc, char **argv)
 {
 	int j, l;
-	int mul;
-	char *num1, *num2;
+	unsigned long int mul;
 
 	if (argc != 3)
 	{
@@ -30,9 +29,10 @@ int main(int argc, char **argv)
 			}
 		}
 	}
+	/*
 	num1 = argv[1];
-	num2 = argv[2];
-	mul = atoi(num1) * atoi(num2);
+	num2 = argv[2];*/
+	mul = atoi(argv[1]) * atoi(argv[2]);
 	printf("%lu\n", mul);
-	return (0);
+return (0);
 }
