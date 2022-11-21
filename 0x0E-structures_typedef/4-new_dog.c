ffd092c7fce097b1faf6b1acfb21fdbe13bbf8df
Hadjara <hadjaramamane61@gmail.com>
2022-11-20 16:51:51 -0800

    task 0-5

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -19,7 +19,7 @@ dog_t *new_dog(char *name, float age, char *owner)
 	if (d == NULL)
 		return (NULL);
 	d->name = strcpy(d->name, name);
-	d.age = age;
+	d->age = age;
 	d->owner = owner;
 	return (d);
 }
