901c57b72f26ea9ddc8cb2c35181183854d42062
Hadjara <hadjaramamane61@gmail.com>
2022-11-20 16:39:15 -0800

    task 0-5

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,18 @@
+#include "dog.h"
+#include <stdio.h>
+
+/**
+ * init_dog - that initialize a variable of type struct dog
+ * @d:  a structure
+ * @name: a pointer
+ * @age: a float
+ * @owner: a pointer
+ * Return: void
+ */
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d->name = name;
+	d.age = age;
+	d->owner = owner;
+}
---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,18 @@
+#include ""dog.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * free_dog - function that frees dog
+ * @d: a structure
+ * Return: void
+ */
+
+void free_dog(dog_t *d)
+{
+	if (d == NULL)
+		return (NULL);
+	free(d->name);
+	free(d.age);
+	free(d->owner);
+	free(d);
+}
