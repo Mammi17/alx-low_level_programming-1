07209b51bdb3dc169b8896a2ae3c96d839ea8b3b
Hadjara <hadjaramamane61@gmail.com>
2022-11-25 20:22:57 -0800

    task 0-4

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -9,14 +9,15 @@
  */
 int main(int argc, char **argv)
 {
-	int num1, num2, (*operator)(int, int);
+	int num1, num2;
+	int (*operator)(int, int);
 
 	if (argc != 4)
 	{
 		printf("Error\n");
 			exit(98);
 	}
-	operator = get_op_function(argv[2]);
+	operator = get_op_func(argv[2]);
 	if (operator == NULL)
 	{
 		printf("Error\n");
@@ -26,7 +27,7 @@ int main(int argc, char **argv)
 	num2 = atoi(argv[3]);
 	printf("%d\n",operator(num1, num2));
 	return (0);
-	if ((argv[2][1] == '/' || argv[2][1] == '%') && argv[3] = 0)
+	if ((argv[2] == '/' || argv[2] == '%') && num2 = 0)
 	{
 		printf("Error\n");
 		exit(100);
