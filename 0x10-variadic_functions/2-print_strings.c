2e53dbc7aa074903c8c25eddee129eee56bb39c7
Hadjara <hadjaramamane61@gmail.com>
2022-12-01 16:19:58 -0800

    task 0-3

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -20,7 +20,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	j = 0;
 	while (j < n)
 	{
-		string = va_arg(note, char *)
+		string = va_arg(note, char *);
 		printf("%s", string);
 		if (string == NULL)
 			printf("(nil)");
