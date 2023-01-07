eebe896749317f55d411797fc0f9c13a3395629e
Hadjara <hadjaramamane61@gmail.com>
2023-01-07 09:45:58 -0800

    task 0-6

---
+++ 0x18-dynamic_libraries/gigam.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+/**
+ * main - enter the code
+ *
+ * Return: an integer
+ */
+
+int main(void)
+{
+	printf("%d\n", _strlen("Congratulation! you win"));
+	return (EXIT_SUCCESS);
+}
---
+++ 0x18-dynamic_libraries/operat.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int addi(int a, int b)
+{
+	return (a + b);
+}
+int divi(int a, int b)
+{
+	return (a / b);
+}
+int subs(int a, int b)
+{
+	return (a - b);
+}
+int modu(int a, int b)
+{
+	return (a % b);
+}
+int mult(int a, int b)
+{
+	return (a * b);
+}
