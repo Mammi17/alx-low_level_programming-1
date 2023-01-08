5b24ecb65cab78f73a7ea76fcc56251dbb02f9b2
Hadjara <hadjaramamane61@gmail.com>
2023-01-07 18:45:19 -0800

    task 0-6

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""Defines an island perimeter measuring function."""
+
+def island_perimeter(grid):
+    """Return the perimeter of an island.
+    The grid represents 0 for water and 1 for land.
+    grid : a list of integers
+    Returns: the perimeter of the island defined in grid """
+
+    larg = 0
+    siz = 0
+
+    for j in range(len(grid)):
+        for l in range(len(grid[0])):
+            if grid[l][j] == 1:
+                siz += 1
+                if (l > 0 and grid[j][l - 1] == 1):
+                    larg += 1
+                if (j > 0 and grid[j - 1][l] == 1):
+                    larg += 1
+    return 4 * siz - larg * 2
---
+++ 0x1C-makefiles/m.h
@@ -0,0 +1,9 @@
+#ifndef M_H
+#define M_H
+
+#include <stdlib.h>
+#include <stdio.h>
+
+void printf_school(void);
+
+#endif
